209adc45d50810321ec41be3416784f720297a4e
hoand <hoaace2003@gmail.com>
2024-09-03 09:47:47 +0700

    update affiliate page

--- src/views/forms/FormWizard.tsx
+++ src/views/forms/FormWizard.tsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Box,
   Stepper,
@@ -10,25 +10,54 @@ import {
   Typography,
   FormControlLabel,
   Alert,
-  Stack
+  Stack,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Input,
+  IconButton,
 } from '@mui/material';
 import PageContainer from 'src/components/container/PageContainer';
 import Breadcrumb from 'src/layouts/full/shared/breadcrumb/Breadcrumb';
-
+import CloseIcon from '@mui/icons-material/Close';
 import CustomTextField from 'src/components/forms/theme-elements/CustomTextField';
 import CustomCheckbox from 'src/components/forms/theme-elements/CustomCheckbox';
 import CustomFormLabel from 'src/components/forms/theme-elements/CustomFormLabel';
 import ParentCard from 'src/components/shared/ParentCard';
 
-const steps = ['Account', 'Profile', 'Finish'];
+const steps = [
+  'Hướng dẫn',
+  'Tài khoản cá nhân',
+  'Thông tin liên hệ',
+  'Tài liệu xác minh',
+  'Kết thúc',
+];
 
 const FormWizard = () => {
+  const [selectedImage1, setSelectedImage1] = useState(null);
+  const [selectedImage2, setSelectedImage2] = useState(null);
+
+  const handleImage1Change = (event) => {
+    setSelectedImage1(URL.createObjectURL(event.target.files[0]));
+  };
+
+  const handleImage2Change = (event) => {
+    setSelectedImage2(URL.createObjectURL(event.target.files[0]));
+  };
+  const handleRemoveImage1 = () => {
+    setSelectedImage1(null);
+  };
+
+  const handleRemoveImage2 = () => {
+    setSelectedImage2(null);
+  };
   const [activeStep, setActiveStep] = React.useState(0);
   const [skipped, setSkipped] = React.useState(new Set());
 
-  const isStepOptional = (step:any) => step === 1;
+  const isStepOptional = (step: any) => step === 1;
 
-  const isStepSkipped = (step:any) => skipped.has(step);
+  const isStepSkipped = (step: any) => skipped.has(step);
 
   const handleNext = () => {
     let newSkipped = skipped;
@@ -67,26 +96,144 @@ const FormWizard = () => {
       case 0:
         return (
           <Box>
-            <CustomFormLabel htmlFor="Name">Name</CustomFormLabel>
-            <CustomTextField id="Name" variant="outlined" fullWidth />
-            <CustomFormLabel htmlFor="Email">Email</CustomFormLabel>
-            <CustomTextField id="Email" type="email" variant="outlined" fullWidth />
-            <CustomFormLabel htmlFor="Password">Password</CustomFormLabel>
-            <CustomTextField id="Password" type="password" variant="outlined" fullWidth />
+            <h1>hello world</h1>
           </Box>
         );
       case 1:
         return (
           <Box>
-            <CustomFormLabel htmlFor="Fname">First Name</CustomFormLabel>
-            <CustomTextField id="Fname" variant="outlined" fullWidth />
-            <CustomFormLabel htmlFor="Lname">Last Name</CustomFormLabel>
-            <CustomTextField id="Lname" type="text" variant="outlined" fullWidth />
-            <CustomFormLabel htmlFor="Address">Address</CustomFormLabel>
-            <CustomTextField id="Address" multiline rows={4} variant="outlined" fullWidth />
+            <Box>
+              <CustomFormLabel htmlFor="banknumber">Số tài khoản</CustomFormLabel>
+              <CustomTextField
+                id="banknumber"
+                variant="outlined"
+                placeholder="Nhập số tài khoản của bạn"
+                fullWidth
+              />
+            </Box>
+            <Box>
+              <CustomFormLabel htmlFor="password">Tên tài khoản</CustomFormLabel>
+              <CustomTextField
+                id="password"
+                type="password"
+                variant="outlined"
+                placeholder="Nhập tên tài khoản của bạn"
+                fullWidth
+              />
+            </Box>
+            <Box>
+              <CustomFormLabel htmlFor="gender">Chọn ngân hàng</CustomFormLabel>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Chọn ngân hàng</InputLabel>
+                <Select labelId="demo-simple-select-label" id="demo-simple-select" label="Gender">
+                  <MenuItem value="mb">Mb bank</MenuItem>
+                  <MenuItem value="tp">TP bank</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
           </Box>
         );
       case 2:
+        return (
+          <Box>
+            <Box>
+              <CustomFormLabel htmlFor="gmail">Gmail</CustomFormLabel>
+              <CustomTextField
+                id="gmail"
+                type="email"
+                variant="outlined"
+                placeholder="Nhập gmail của bạn"
+                fullWidth
+              />
+            </Box>
+            <Box>
+              <CustomFormLabel htmlFor="phonenumber">Số điện thoại</CustomFormLabel>
+              <CustomTextField
+                id="phonenumber"
+                type="tel"
+                variant="outlined"
+                placeholder="Nhập số điện thoại của bạn"
+                fullWidth
+              />
+            </Box>
+            <Box>
+              <CustomFormLabel htmlFor="address">Địa chỉ</CustomFormLabel>
+              <CustomTextField
+                id="address"
+                type="text"
+                variant="outlined"
+                placeholder="Nhập địa chỉ của bạn"
+                fullWidth
+              />
+            </Box>
+          </Box>
+        );
+      case 3:
+        return (
+          <Box>
+            <Box mb={2}>
+              {selectedImage1 && (
+                <Box mb={2} position="relative">
+                  <Typography variant="subtitle1">Mặt trước căn cước công dân:</Typography>
+                  <img
+                    src={selectedImage1}
+                    alt="Selected 1"
+                    style={{ width: '100%', maxHeight: '200px', objectFit: 'cover' }}
+                  />
+                  <IconButton
+                    size="small"
+                    onClick={handleRemoveImage1}
+                    sx={{ position: 'absolute', top: 0, right: 0 }}
+                  >
+                    <CloseIcon />
+                  </IconButton>
+                </Box>
+              )}
+              <CustomFormLabel htmlFor="image1">Mặt trước căn cước công dân</CustomFormLabel>
+              <Button variant="contained" component="label" color="primary">
+                Choose File
+                <Input
+                  id="image1"
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImage1Change}
+                  style={{ display: 'none' }}
+                />
+              </Button>
+            </Box>
+            <Box mb={2}>
+              {selectedImage2 && (
+                <Box mb={2} position="relative">
+                  <Typography variant="subtitle1">Mặt sau căn cước công dân:</Typography>
+                  <img
+                    src={selectedImage2}
+                    alt="Selected 2"
+                    style={{ width: '100%', maxHeight: '200px', objectFit: 'cover' }}
+                  />
+                  <IconButton
+                    size="small"
+                    onClick={handleRemoveImage2}
+                    sx={{ position: 'absolute', top: 0, right: 0 }}
+                  >
+                    <CloseIcon />
+                  </IconButton>
+                </Box>
+              )}
+              <CustomFormLabel htmlFor="image2">Mặt sau căn cước công dân</CustomFormLabel>
+              <Button variant="contained" component="label" color="primary">
+                Choose File
+                <Input
+                  id="image2"
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImage2Change}
+                  style={{ display: 'none' }}
+                />
+              </Button>
+            </Box>
+          </Box>
+        );
+      case 4:
         return (
           <Box pt={3}>
             <Typography variant="h5">Terms and condition</Typography>
@@ -121,9 +268,9 @@ const FormWizard = () => {
               const labelProps: {
                 optional?: React.ReactNode;
               } = {};
-              if (isStepOptional(index)) {
-                labelProps.optional = <Typography variant="caption">Optional</Typography>;
-              }
+              // if (isStepOptional(index)) {
+              //   labelProps.optional = <Typography variant="caption">Optional</Typography>;
+              // }
               if (isStepSkipped(index)) {
                 stepProps.completed = false;
               }
@@ -138,9 +285,7 @@ const FormWizard = () => {
           {activeStep === steps.length ? (
             <>
               <Stack spacing={2} mt={3}>
-                <Alert severity="success">
-                  All steps completed - you&apos;re finished
-                </Alert>
+                <Alert severity="success">All steps completed - you&apos;re finished</Alert>
 
                 <Box textAlign="right">
                   <Button onClick={handleReset} variant="contained" color="error">
