a6843b2560fea00b4fc44652eb741fe62962b83f
truong_napan <truongnapan111@gmail.com>
2024-09-04 11:02:50 +0700

    update

--- src/views/authentication/Updating.tsx
+++ src/views/authentication/Updating.tsx
@@ -1,30 +1,48 @@
-import { Box, Typography, CircularProgress, Container, Button } from '@mui/material';
+import { Box, Typography, Container, Button } from '@mui/material';
 import React from 'react';
-import UpdatingImg from 'src/assets/images/backgrounds/updating.svg';
+import bot from 'src/assets/images/backgrounds/bot.svg';
 import { Link } from 'react-router-dom';
 
 const Updating = () => {
   return (
     <Box
-    display="flex"
-    flexDirection="column"
-    height="100vh"
-    textAlign="center"
-    justifyContent="center"
-  >
-    <Container maxWidth="md">
-      <img src={UpdatingImg} alt="404" style={{ width: '100%', maxWidth: '500px' }} />
-      <Typography align="center" variant="h1" mb={4}>
-        Đang cập nhật...
-      </Typography>
-      <Typography align="center" variant="h6" mb={4}>
-        Website đang được xây dựng. Vui lòng quay lại sau!
-      </Typography>
-      <Button color="primary" variant="contained" component={Link} to="/" disableElevation>
-        Trở về trang chủ
-      </Button>
-    </Container>
-  </Box>
+      display="flex"
+      flexDirection="column"
+      height="100vh"
+      textAlign="center"
+      justifyContent="center"
+    >
+      <Container maxWidth="md">
+        <Box
+          component="img"
+          src={bot}
+          alt="404"
+          sx={{
+            width: '100%',
+            maxWidth: '400px',
+            animation: 'spin 4s linear infinite',
+            '@keyframes spin': {
+              '0%': {
+                transform: 'rotateY(0deg)',
+              },
+              '100%': {
+                transform: 'rotateY(360deg)',
+              },
+            },
+          }}
+        />
+        <br />
+        <Typography align="center" variant="h1" mb={4}>
+          Đang cập nhật...
+        </Typography>
+        <Typography align="center" variant="h6" mb={4}>
+          Website đang được xây dựng. Vui lòng quay lại sau!
+        </Typography>
+        <Button color="primary" variant="contained" component={Link} to="/" disableElevation>
+          Trở về trang chủ
+        </Button>
+      </Container>
+    </Box>
   );
 };
 
