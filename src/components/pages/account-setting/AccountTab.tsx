b5e3ff1da3d5ce1dd3d246233843bb012e4e5ffd
ntoarn <nqton301004@gmail.com>
2024-09-06 09:02:27 +0700

    toan

--- src/components/pages/account-setting/AccountTab.tsx
+++ src/components/pages/account-setting/AccountTab.tsx
@@ -1,13 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import React from 'react';
-import { CardContent, Grid, Typography, MenuItem, Box, Avatar, Button, Stack } from '@mui/material';
+import { CardContent, Grid, Typography, Box, Button, Stack } from '@mui/material';
 
 // components
 import BlankCard from '../../shared/BlankCard';
 import CustomTextField from '../../forms/theme-elements/CustomTextField';
 import CustomFormLabel from '../../forms/theme-elements/CustomFormLabel';
-import CustomSelect from '../../forms/theme-elements/CustomSelect';
 
 // images
 import user1 from 'src/assets/images/profile/user-1.jpg';
@@ -38,44 +37,10 @@ const AccountTab = () => {
 
   return (
     <Grid container spacing={3}>
-      {/* Change Profile */}
-      <Grid item xs={12} lg={6}>
-        <BlankCard>
-          <CardContent>
-            <Typography variant="h5" mb={1}>
-              Chỉnh sửa trang cá nhân
-            </Typography>
-            <Typography color="textSecondary" mb={3}>
-              Cập nhật ảnh đại diện
-            </Typography>
-            <Box textAlign="center" display="flex" justifyContent="center">
-              <Box>
-                <Avatar
-                  src={user1}
-                  alt={user1}
-                  sx={{ width: 120, height: 120, margin: '0 auto' }}
-                />
-                <Stack direction="row" justifyContent="center" spacing={2} my={3}>
-                  <Button variant="contained" color="primary" component="label">
-                    Tải lên
-                    <input hidden accept="image/*" multiple type="file" />
-                  </Button>
-                  <Button variant="outlined" color="error">
-                    Cài lại
-                  </Button>
-                </Stack>
-                <Typography variant="subtitle1" color="textSecondary" mb={4}>
-                  Cho phép JPG, GIF hoặc PNG. Kích thước tối đa 800K
-                </Typography>
-              </Box>
-            </Box>
-          </CardContent>
-        </BlankCard>
-      </Grid>
       {/*  Change Password */}
-      <Grid item xs={12} lg={6}>
+      {/* <Grid item xs={12} lg={6}> */}
         <BlankCard>
-          <CardContent>
+          <CardContent >
             <Typography variant="h5" mb={1}>
               Đổi mật khẩu
             </Typography>
@@ -116,122 +81,21 @@ const AccountTab = () => {
                 fullWidth
                 type="password"
               />
+              {/* Submit Button */}
+              <Stack direction="row" spacing={2} sx={{ justifyContent: 'end', mt: 3 }}>
+                <Button size="large" variant="contained" color="primary" type="submit">
+                  Lưu
+                </Button>
+                <Button size="large" variant="text" color="error">
+                  Hủy
+                </Button>
+              </Stack>
             </form>
           </CardContent>
         </BlankCard>
-      </Grid>
+      {/* </Grid> */}
       {/* Edit Details */}
-      <Grid item xs={12}>
-        <BlankCard>
-          <CardContent>
-            <Typography variant="h5" mb={1}>
-              Chi tiết trang cá nhân
-            </Typography>
-            <Typography color="textSecondary" mb={3}>
-              Chỉnh sửa thông tin cá nhân tại đây
-            </Typography>
-            <form>
-              <Grid container spacing={3}>
-                <Grid item xs={12} sm={6}>
-                  <CustomFormLabel
-                    sx={{
-                      mt: 0,
-                    }}
-                    htmlFor="text-name"
-                  >
-                    Họ và tên
-                  </CustomFormLabel>
-                  <CustomTextField
-                    id="text-name"
-                    value="Nguyễn Đăng Hòa"
-                    variant="outlined"
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  {/* 3 */}
-                  <CustomFormLabel
-                    sx={{
-                      mt: 0,
-                    }}
-                    htmlFor="text-location"
-                  >
-                    Giới tính
-                  </CustomFormLabel>
-                  <CustomSelect
-                    fullWidth
-                    id="text-location"
-                    variant="outlined"
-                    value={location}
-                    onChange={handleChange1}
-                  >
-                    {locations.map((option) => (
-                      <MenuItem key={option.value} value={option.value}>
-                        {option.label}
-                      </MenuItem>
-                    ))}
-                  </CustomSelect>
-                </Grid>
-
-                <Grid item xs={12} sm={6}>
-                  {/* 5 */}
-                  <CustomFormLabel
-                    sx={{
-                      mt: 0,
-                    }}
-                    htmlFor="text-email"
-                  >
-                    Gmail
-                  </CustomFormLabel>
-                  <CustomTextField
-                    id="text-email"
-                    value="infor@gmail.com"
-                    variant="outlined"
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  {/* 6 */}
-                  <CustomFormLabel
-                    sx={{
-                      mt: 0,
-                    }}
-                    htmlFor="text-phone"
-                  >
-                    Số điện thoại
-                  </CustomFormLabel>
-                  <CustomTextField
-                    id="text-phone"
-                    value="+981 522 873"
-                    variant="outlined"
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  {/* 7 */}
-                  <CustomFormLabel
-                    sx={{
-                      mt: 0,
-                    }}
-                    htmlFor="text-address"
-                  >
-                    Ngày sinh
-                  </CustomFormLabel>
-                  <CustomTextField id="text-address" variant="outlined" type="date" fullWidth />
-                </Grid>
-              </Grid>
-            </form>
-          </CardContent>
-        </BlankCard>
-        <Stack direction="row" spacing={2} sx={{ justifyContent: 'end' }} mt={3}>
-          <Button size="large" variant="contained" color="primary">
-            Lưu
-          </Button>
-          <Button size="large" variant="text" color="error">
-            Hủy
-          </Button>
-        </Stack>
-      </Grid>
+      {/* ... other code ... */}
     </Grid>
   );
 };
