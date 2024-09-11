a0006ab8859d5f76d872528b1e115a026ee9303e
truong_napan <truongnapan111@gmail.com>
2024-09-11 15:12:01 +0700

    fix

--- src/components/apps/assistant/AssistantEditor/Strategy.tsx
+++ src/components/apps/assistant/AssistantEditor/Strategy.tsx
@@ -7,12 +7,10 @@ import BlankCard from '../AssistantEditor/BlankCard';
 
 const Strategy = () => {
     return (
-        
     <Box>
       <CardContent sx={{ p: '30px' }}>
         <Box textAlign="center">
           <img src={starBg} alt="star" width={130} />
-
           <Typography variant="h5">Golden Rank</Typography>
         </Box>
       </CardContent>
