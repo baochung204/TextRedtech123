44e5e18d581181dd111475901fdce522b8ee1704
BaoChung-Cloud <chungkbph38477@fpt.edu.vn>
2024-09-11 09:26:41 +0700

    new code

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { useSelector } from 'src/store/Store';
 import { ThemeSettings } from './theme/Theme';
 import RTL from './layouts/full/shared/customizer/RTL';
 import ScrollToTop from './components/shared/ScrollToTop';
-import Router from './theme/Router';
+import Router from './routers/Router';
 import { AppState } from './store/Store';
 
 function App() {
