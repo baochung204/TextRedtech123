2f87bfb6ce57b642e7b4adb92a1be7663826cf3e
khaidinhh1234 <khaindph36466@fpt.edu.vn>
2024-09-19 18:01:08 +0700

    khaidan

--- Frontend/src/components/widgets/cards/TopCard.tsx
+++ Frontend/src/components/widgets/cards/TopCard.tsx
@@ -1,9 +1,6 @@
 import { Box, Grid, Typography, styled } from '@mui/material';
-<<<<<<< HEAD
-import React, { useEffect, useState } from 'react';
-=======
+
 import { useEffect, useState } from 'react';
->>>>>>> 9a4f29ca058ba07ba7cfccc2dd406ec1f657bef8
 
 interface StyleProps {
   bgColor: string;
@@ -19,68 +16,6 @@ interface TopCardProps {
 }
 
 const BoxStyled = styled(Box)(() => ({
-<<<<<<< HEAD
-    padding: '30px',
-    transition: '0.1s ease-in',
-    cursor: 'pointer',
-    color: 'inherit',
-    '&:hover': {
-        transform: 'scale(1.03)',
-    },
-}));
-
-const TopCard = ({ dataSource, totalColumn }: TopCardProps) => {
-    const [total, setTotal] = useState<number | null>(null);
-
-    useEffect(() => {
-        if (totalColumn) {
-            const parsedValue = parseInt(totalColumn, 10);
-            if (!isNaN(parsedValue) && parsedValue > 0) {
-                setTotal(12 / parsedValue);
-            } else {
-                setTotal(null); // Or set to a default value if needed
-            }
-        } else {
-            setTotal(null); // Or set to a default value if needed
-        }
-    }, [totalColumn]);
-
-    console.log(total);
-
-    return (
-        <Grid container spacing={total ?? 2}>
-            {dataSource.map((item, index) => (
-                <Grid item lg={3} sm={6} xs={12} key={index}>
-                    <BoxStyled
-                        sx={{
-                            backgroundColor: item.bgColor,
-                            color: item.color
-                        }}
-                    >
-                        <Grid container>
-                            <Grid
-                                item
-                                xs={3}
-                                sx={{
-                                    display: 'flex',
-                                    alignItems: 'center'
-                                }}
-                            >
-                                {item.icons}
-                            </Grid>
-                            <Grid item xs={9}>
-                                <Typography variant="h4">{item.title}</Typography>
-                                <Typography variant="h5">{item.total}</Typography>
-                            </Grid>
-                        </Grid>
-                    </BoxStyled>
-                </Grid>
-            ))}
-        </Grid>
-    );
-};
-
-=======
   padding: '24px',
   transition: '0.1s ease-in',
   cursor: 'pointer',
@@ -134,5 +69,4 @@ const TopCard = ({ dataSource, totalColumn }: TopCardProps) => {
   );
 };
 
->>>>>>> 9a4f29ca058ba07ba7cfccc2dd406ec1f657bef8
 export default TopCard;
