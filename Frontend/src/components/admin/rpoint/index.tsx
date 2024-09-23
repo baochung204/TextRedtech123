82470ac31c3a4bcf25794588dc7ab34c1a9074c7
BaoChung-Cloud <chungkbph38477@fpt.edu.vn>
2024-09-23 17:37:34 +0700

    new code

--- Frontend/src/components/admin/rpoint/index.tsx
+++ Frontend/src/components/admin/rpoint/index.tsx
@@ -75,26 +75,26 @@ const columns = [
     title: 'Ngày tạo',
     dataIndex: 'createDate',
   },
-  {
-    title: 'Hành động',
-    // dataIndex: 'hanhdong',
-    render: (row: PropsTable) => {
-      return (
-        <Grid container spacing={7}>
-          <Grid item xs={6}>
-            <Button variant="contained" onClick={() => console.log(row.id)}>
-              V
-            </Button>
-          </Grid>
-          <Grid item xs={6}>
-            <Button variant="contained" color="error">
-              X
-            </Button>
-          </Grid>
-        </Grid>
-      );
-    },
-  },
+  // {
+  //   title: 'Hành động',
+  //   // dataIndex: 'hanhdong',
+  //   render: (row: PropsTable) => {
+  //     return (
+  //       <Grid container spacing={7}>
+  //         <Grid item xs={6}>
+  //           <Button variant="contained" onClick={() => console.log(row.id)}>
+  //             V
+  //           </Button>
+  //         </Grid>
+  //         <Grid item xs={6}>
+  //           <Button variant="contained" color="error">
+  //             X
+  //           </Button>
+  //         </Grid>
+  //       </Grid>
+  //     );
+  //   },
+  // },
 ];
 
 const PublisherTablePage: React.FC = () => {
