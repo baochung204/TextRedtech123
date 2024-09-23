d6cab333d298e1b13df7404a94885f0bf4c7bd33
ntoarn <nqton301004@gmail.com>
2024-09-23 16:32:48 +0700

    update

--- Frontend/src/views/admin/blog/_components/TableBlog.tsx
+++ Frontend/src/views/admin/blog/_components/TableBlog.tsx
@@ -1,29 +1,32 @@
 import { Favorite, Visibility } from '@mui/icons-material';
 import {
   Avatar,
-  Button,
+  Chip,
   Grid,
+  IconButton,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableHead,
+  TablePagination,
   TableRow,
-  Typography,
+  Typography
 } from '@mui/material';
-import { useState } from 'react';
+import { IconEdit, IconEye, IconTrash } from '@tabler/icons-react';
+import React, { useState } from 'react';
 import BlogTable from '../data/datablog';
 
 interface PropsHeadTable {
   head: string;
 }
-
+const tagColors = ['#FF5733', '#33FF57', '#3357FF', '#FF33A1', '#FFC300', '#DAF7A6', '#C70039'];
 const HeadTable: PropsHeadTable[] = [
   { head: 'ID' },
   { head: 'Ngày tạo' },
   { head: 'Ảnh' },
-  { head: 'Tiêu đề' },
   { head: 'Tác giả' },
+  { head: 'Tiêu đề' },
   { head: 'Tags' },
   { head: 'Đường dẫn url' },
   { head: 'Mô tả' },
@@ -36,169 +39,142 @@ const HeadTable: PropsHeadTable[] = [
 ];
 
 const TableBlog = () => {
-  const [page] = useState(0);
-  const [rowsPerPage] = useState(5);
-  // const [key, setKey] = useState<string | null>(null);
-  // const handleChangePage = (newPage: number) => {
-  //   setPage(newPage);
-  // };
-
-  // const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setRowsPerPage(parseInt(event.target.value, 10));
-  //   setPage(0);
-  // };
-
+  const [key, setKey] = useState<string | null>(null);
+  key && console.log(key);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
   const paginatedData = BlogTable.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
-
-  // Placeholder functions for button actions
-  const handleView = (id: string) => {
-    console.log('View blog with ID:', id);
+  const getColorForTag = (index: number) => {
+    return tagColors[index % tagColors.length]; // Cycle through the colors
+  };
+  const handleChangePage = (newPage: number) => {
+    setPage(newPage);
   };
 
-  const handleEdit = (id: string) => {
-    console.log('Edit blog with ID:', id);
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
   };
-
-  const handleDelete = (id: string) => {
-    console.log('Delete blog with ID:', id);
-  };
-
   return (
-    <>
-      <TableContainer>
-        <Table>
-          <TableHead>
-            <TableRow>
-              {HeadTable.map((item, index) => (
-                <TableCell key={index} sx={{ whiteSpace: 'nowrap' }}>
-                  <Typography variant="h6">{item.head}</Typography>
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {HeadTable.map((item, index) => (
+              <TableCell key={index} sx={{ whiteSpace: 'nowrap' }}>
+                <Typography variant="h6">{item.head}</Typography>
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {paginatedData.map((item, index) => {
+            return (
+              <TableRow key={index}>
+                <TableCell>
+                  <Typography variant="subtitle2">{item.id}</Typography>
                 </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {paginatedData.map((item, index) => {
-              return (
-                <TableRow key={index}>
-                  <TableCell>
-                    <Typography variant="subtitle2">{item.id}</Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Typography variant="subtitle2">
-                      {item.createdAt.toLocaleDateString()}
-                    </Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Avatar
-                      src={item.avt}
-                      variant="rounded"
-                      alt={item.avt}
-                      sx={{ width: 48, height: 48 }}
-                    />
-                  </TableCell>
-                  <TableCell
-                    sx={{
-                      maxWidth: 150,
-                      overflow: 'hidden',
-                      textOverflow: 'ellipsis',
-                      whiteSpace: 'nowrap',
-                    }}
+                <TableCell>
+                  <Typography variant="subtitle2">{item.createdAt.toLocaleDateString()}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Avatar src={item.avt} variant="rounded" alt={item.avt} sx={{ width: 48, height: 48 }} />
+                </TableCell>
+                <TableCell sx={{ minWidth: 200 }}>
+                  <Typography variant="subtitle2">{item.author}</Typography>
+                </TableCell>
+                <TableCell sx={{ minWidth: 200 }}>
+                  <Typography
+                    variant="subtitle2"
+                    sx={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'normal', wordWrap: 'break-word' }}
                   >
-                    <Typography variant="subtitle2">{item.title}</Typography>
-                  </TableCell>
-                  <TableCell sx={{ maxWidth: 100, overflow: 'hidden' }}>
-                    <Typography variant="subtitle2">{item.author}</Typography>
-                  </TableCell>
-                  <TableCell sx={{ maxWidth: 100, overflow: 'hidden' }}>
-                    <Typography variant="subtitle2">{item.tags}</Typography>
-                  </TableCell>
-                  <TableCell sx={{ maxWidth: 150, overflow: 'hidden', textOverflow: 'ellipsis' }}>
-                    <Typography variant="subtitle2">{item.url}</Typography>
-                  </TableCell>
-                  <TableCell
-                    sx={{
-                      maxWidth: 150,
-                      overflow: 'hidden',
-                      textOverflow: 'ellipsis',
-                      whiteSpace: 'nowrap',
-                    }}
+                    {item.title}
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                {item.tags.split(',').map((tag, tagIndex) => (
+                  <Chip
+                    key={tagIndex}
+                    label={tag}
+                    sx={{ backgroundColor: getColorForTag(tagIndex), color: '#fff', margin: '2px' }}
+                  />
+                ))}
+              </TableCell>
+                <TableCell sx={{ minWidth: 150 }}>
+                  <Typography variant="subtitle2" sx={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+                    {item.url}
+                  </Typography>
+                </TableCell>
+                <TableCell sx={{ minWidth: 200 }}>
+                  <Typography
+                    variant="subtitle2"
+                    sx={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'normal', wordWrap: 'break-word' }}
                   >
-                    <Typography variant="subtitle2">{item.description}</Typography>
-                  </TableCell>
-                  <TableCell
-                    sx={{
-                      maxWidth: 200,
-                      overflow: 'hidden',
-                      textOverflow: 'ellipsis',
-                      whiteSpace: 'nowrap',
-                    }}
+                    {item.description}
+                  </Typography>
+                </TableCell>
+                <TableCell sx={{ minWidth: 250 }}>
+                  <Typography
+                    variant="subtitle2"
+                    sx={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'normal', wordWrap: 'break-word' }}
                   >
-                    <Typography variant="subtitle2">{item.content}</Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Typography variant="subtitle2">{item.pricePoint}</Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Typography variant="subtitle2">{item.status}</Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Grid container alignItems="center" spacing={1}>
-                      <Grid item>
-                        <Visibility color="action" />
-                      </Grid>
-                      <Grid item>
-                        <Typography variant="subtitle2">{item.view}</Typography>
-                      </Grid>
+                    {item.content}
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="subtitle2">{item.pricePoint}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="subtitle2">{item.status}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Grid container alignItems="center" spacing={1}>
+                    <Grid item>
+                      <Typography variant="subtitle2">{item.view}</Typography>
                     </Grid>
-                  </TableCell>
-                  <TableCell>
-                    <Grid container alignItems="center" spacing={1}>
-                      <Grid item>
-                        <Favorite color="error" />
-                      </Grid>
-                      <Grid item>
-                        <Typography variant="subtitle2">{item.like}</Typography>
-                      </Grid>
+                    <Grid item>
+                      <Visibility color="action" />
                     </Grid>
-                  </TableCell>
-                  <TableCell>
-                    <Grid container spacing={1}>
-                      <Grid item>
-                        <Button
-                          variant="contained"
-                          style={{ backgroundColor: 'green', color: 'white' }}
-                          onClick={() => handleView(item.id)}
-                        >
-                          Xem
-                        </Button>
-                      </Grid>
-                      <Grid item>
-                        <Button
-                          variant="contained"
-                          color="primary"
-                          onClick={() => handleEdit(item.id)}
-                        >
-                          Sửa
-                        </Button>
-                      </Grid>
-                      <Grid item>
-                        <Button
-                          variant="contained"
-                          style={{ backgroundColor: 'red', color: 'white' }}
-                          onClick={() => handleDelete(item.id)}
-                        >
-                          Xóa
-                        </Button>
-                      </Grid>
+                  </Grid>
+                </TableCell>
+                <TableCell>
+                  <Grid container alignItems="center" spacing={1}>
+                    <Grid item>
+                      <Typography variant="subtitle2">{item.like}</Typography>
                     </Grid>
-                  </TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
+                    <Grid item>
+                      <Favorite color="error" />
+                    </Grid>
+                  </Grid>
+                </TableCell>
+                <TableCell sx={{ whiteSpace: 'nowrap' }}>
+                  <IconButton onClick={() => setKey(item.id)}>
+                    <IconEye stroke={2} style={{ color: '#b1ffb3' }} />
+                  </IconButton>
+                  <IconButton>
+                    <IconEdit stroke={2} style={{ color: '#5D87FF' }} />
+                  </IconButton>
+                  <IconButton>
+                    <IconTrash stroke={2} style={{ color: '#FA896B' }} />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+      <TablePagination
+              rowsPerPageOptions={[5, 10, 25]}
+              component="div"
+              count={BlogTable.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={(_event, newPage) => handleChangePage(newPage)}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              labelRowsPerPage="Số hàng trên mỗi trang"
+            />
+    </TableContainer>
+    
   );
 };
 
--- Frontend/src/views/apps/customerList/CustomerList.tsx
+++ Frontend/src/views/apps/customerList/CustomerList.tsx
@@ -17,239 +17,114 @@ import { TransitionProps } from '@mui/material/transitions';
 import * as React from 'react';
 import CustomerTable from 'src/components/tables/CustomerTable';
 import PopupAdd from './PopupAdd';
-// import { TabPanel } from '@mui/lab';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { IconPlus, IconSearch } from '@tabler/icons-react';
 import BannerPage from 'src/layouts/full/shared/breadcrumb/BannerPage';
+import PageContainer from 'src/components/container/PageContainer';
+import  ChildCard  from 'src/components/shared/ChildCard';
+import { TabPanel, TabContext } from '@mui/lab';
+
 const Transition = React.forwardRef<unknown, TransitionProps & { children: React.ReactElement }>(
-  function Transition(props, ref) {
-    return (
-      <Slide direction="up" ref={ref} {...props}>
-        {props.children}
-      </Slide>
-    );
-  },
+  (props, ref) => (
+    <Slide direction="up" ref={ref} {...props}>
+      {props.children}
+    </Slide>
+  ),
 );
 
 const BCrumb = [
   { to: '/', title: 'Trang Chủ' },
   { to: '/apps/blog/posts', title: 'Danh sách khách hàng' },
 ];
-interface FilmsData {
-  id: number;
-  title: string;
-}
-const FilmsData: FilmsData[] = [
-  { id: 1, title: 'File' },
-  { id: 2, title: 'Dung lượng' },
-  { id: 3, title: 'Functions' },
-  { id: 4, title: 'Token huấn luyện' },
-  { id: 5, title: 'Ngày tạo' },
-  { id: 6, title: 'Vòng quay trung bình' },
-  { id: 7, title: 'khách hàng' },
-  { id: 8, title: 'Đơn hàng' },
-  { id: 9, title: 'CVR' },
-  { id: 10, title: 'GMV' },
-  { id: 11, title: 'Chi phí' },
-  { id: 12, title: 'Chi phí/Doanh thu' },
-  { id: 13, title: 'Chi phí/Đơn hàng' },
-  { id: 14, title: 'Chi phí/Khách hàng' },
-  { id: 15, title: 'Chiến lược' },
-];
+
 const CustomerList = () => {
   const [selectedStartDate, setSelectedStartDate] = React.useState<Date | null>(null);
   const [selectedEndDate, setSelectedEndDate] = React.useState<Date | null>(null);
-  // const [filterColumn, setFilterColumn] = React.useState('');
-  // const [searchText, setSearchText] = React.useState('');
-  // const [selectedItems, setSelectedItems] = React.useState<number[]>([]);
-
-  // const handleItemClick = (id: number) => {
-  //   setSelectedItems((prev) =>
-  //     prev.includes(id) ? prev.filter((item) => item !== id) : [...prev, id],
-  //   );
-  // };
-
-  // const [iconIndex, setIconIndex] = React.useState<number>(0);
-  // const icons = [SwapVertIcon, SouthIcon, NorthIcon];
-
-  // const handleClickIcon = () => {
-  //   setIconIndex((pre) => (pre + 1) % icons.length);
-  // };
   const [isPopupOpen, setIsPopupOpen] = React.useState(false);
 
-  // Function mở popup
-
-  // Function đóng popup
   const handleClosePopup = () => {
     setIsPopupOpen(false);
   };
 
   return (
-    <div className="customer-list-container">
-      {/* Breadcrumb */}
-      {/* <Breadcrumb title="Blog Detail" items={BCrumb} /> */}
+    <PageContainer>
       <BannerPage title="Danh sách khách hàng" items={BCrumb} />
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Grid container sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Grid item xs={4} sm={4} md={4}>
-              <Grid container sx={{ display: 'flex', alignItems: 'center' }}>
-                <Grid item xs={2} sx={{ display: 'flex', alignItems: 'center' }}>
-                  <Tooltip title="Thêm mới sản phẩm" sx={{ mb: '15px' }}>
-                    <Fab size="small" color="secondary" aria-label="plus" sx={{ my: 'auto' }}>
-                      <IconPlus width={18} />
-                    </Fab>
-                  </Tooltip>
+      <ChildCard sx={{ border: 'none' }} sx1={{ padding: 0 }}>
+        <TabContext value="1">
+          <Box>
+            <TabPanel value="1" sx={{ p: 0, mt: 2 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <Grid container spacing={2} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                    <Grid item xs={12} sm={4}>
+                      <Grid container sx={{ display: 'flex', alignItems: 'center' }}>
+                        {/* <Grid item xs={1.5} sx={{ display: 'flex', alignItems: 'center' }}>
+                          <Tooltip title="Thêm mới khách hàng" onClick={handleClosePopup}>
+                            <Fab size="small" color="secondary" aria-label="plus" sx={{ my: 'auto' }}>
+                              <IconPlus width={18} />
+                            </Fab>
+                          </Tooltip>
+                        </Grid> */}
+                        <Grid item xs={10}>
+                          <TextField
+                            id="outlined-search"
+                            placeholder="Tìm kiếm khách hàng"
+                            size="small"
+                            variant="outlined"
+                            InputProps={{
+                              startAdornment: (
+                                <InputAdornment position="start">
+                                  <IconSearch size="20" />
+                                </InputAdornment>
+                              ),
+                            }}
+                            fullWidth
+                          />
+                        </Grid>
+                      </Grid>
+                    </Grid>
+
+                    <Grid item xs={10} sm={4}>
+                      <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                        <LocalizationProvider dateAdapter={AdapterDateFns}>
+                          <DatePicker
+                            value={selectedStartDate}
+                            onChange={setSelectedStartDate}
+                            renderInput={(params) => <TextField {...params} size="small" fullWidth />}
+                          />
+                          <Typography>tới</Typography>
+                          <DatePicker
+                            value={selectedEndDate}
+                            onChange={setSelectedEndDate}
+                            renderInput={(params) => <TextField {...params} size="small" fullWidth />}
+                          />
+                        </LocalizationProvider>
+                      </Box>
+                    </Grid>
+                  </Grid>
                 </Grid>
-                <Grid item xs={10}>
-                  <TextField
-                    id="outlined-search"
-                    placeholder="Tìm kiếm thông báo"
-                    size="small"
-                    type="search"
-                    variant="outlined"
-                    inputProps={{ 'aria-label': 'Search Followers' }}
-                    sx={{ fontSize: { xs: '10px', sm: '16px', md: '16px' } }}
-                    InputProps={{
-                      startAdornment: (
-                        <InputAdornment position="start">
-                          <IconSearch size="20" />
-                        </InputAdornment>
-                      ),
-                    }}
-                    fullWidth={true}
-                  />
+
+                <Grid item xs={12}>
+                  <CustomerTable />
                 </Grid>
               </Grid>
-            </Grid>
+            </TabPanel>
+          </Box>
+        </TabContext>
+      </ChildCard>
 
-            <Grid item xs={4}>
-              <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                  <DatePicker
-                    value={selectedStartDate}
-                    onChange={setSelectedStartDate}
-                    renderInput={(params: any) => <TextField {...params} />}
-                  />
-                  <Typography>tới</Typography>
-                  <DatePicker
-                    value={selectedEndDate}
-                    onChange={setSelectedEndDate}
-                    renderInput={(params: any) => <TextField {...params} />}
-                  />
-                </LocalizationProvider>
-              </Box>
-            </Grid>
-          </Grid>
-        </Grid>
-
-        <Grid item xs={12}>
-          {' '}
-          <CustomerTable />
-        </Grid>
-      </Grid>
-      {/* Action Buttons and Filters */}
-      {/* Action Buttons and Filters */}
-      {/* <Box
-        className="actions-and-filters"
-        sx={{
-          marginBottom: '20px',
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      > */}
-      {/* Nút tạo đơn hàng và thanh tìm kiếm */}
-      {/* <Box sx={{ display: 'flex', alignItems: 'center' }}> */}
-      {/* <Tooltip title="Tạo đơn hàng">
-            <Fab
-              color="primary"
-              aria-label="add"
-              size="small" // Set the size to "small"
-              sx={{ marginRight: '30px' }}
-              onClick={handleOpenPopup}
-            >
-              <FaPlus />
-            </Fab>
-          </Tooltip> */}
-
-      {/* Thanh tìm kiếm với icon */}
-      {/* <TextField
-            sx={{
-              width: '200px',
-              marginRight: '40px',
-              '& .MuiOutlinedInput-root': {
-                borderRadius: '10px',
-                backgroundColor: '#fff',
-                '&:hover fieldset': {
-                  borderColor: '#3f51b5',
-                },
-              },
-            }}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <IconSearch size="1.1rem" />
-                </InputAdornment>
-              ),
-            }}
-            placeholder="Tìm kiếm"
-            size="small"
-          /> */}
-
-      {/* <CustomSelect
-            labelId="column-filter"
-            id="column-filter"
-            size="small"
-            value={1} // Setting the first value as default
-            sx={{ marginRight: '30px' }}
-          >
-            <MenuItem value={1}>Sửa đổi cột</MenuItem>
-          </CustomSelect>
-
-          <CustomSelect
-            labelId="column-sort"
-            id="column-sort"
-            size="small"
-            value={1} // Setting the first value as default
-            sx={{ marginRight: '20px' }}
-          >
-            <MenuItem value={1}>Bộ lọc</MenuItem>
-          </CustomSelect> */}
-      {/* </Box> */}
-
-      {/* Time Filter and Refresh Icon */}
-      {/* <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <DatePicker
-              value={selectedStartDate}
-              onChange={(newDate) => setSelectedStartDate(newDate)}
-              renderInput={(params) => <CustomTextField {...params} sx={{ marginRight: '10px' }} />}
-            />
-            <Typography sx={{ marginRight: '10px' }}>tới</Typography>
-            <DatePicker
-              value={selectedEndDate}
-              onChange={(newDate) => setSelectedEndDate(newDate)}
-              renderInput={(params) => <CustomTextField {...params} sx={{ marginRight: '10px' }} />}
-            />
-          </LocalizationProvider>
-        </Box>
-      </Box> */}
-      {/* Bảng khách hàng */}
-
-      {/* Popup Thêm đơn hàng */}
       <Dialog
         open={isPopupOpen}
         onClose={handleClosePopup}
         fullWidth
         maxWidth="lg"
-        TransitionComponent={Transition} // Thêm dòng này để sử dụng hiệu ứng slide
+        TransitionComponent={Transition}
         keepMounted
       >
-        <DialogTitle>Thêm affilaite</DialogTitle>
+        <DialogTitle>Thêm khách hàng</DialogTitle>
         <DialogContent>
-          <PopupAdd /> {/* Gọi component PopupAdd */}
+          <PopupAdd />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClosePopup}>Hủy</Button>
@@ -258,7 +133,7 @@ const CustomerList = () => {
           </Button>
         </DialogActions>
       </Dialog>
-    </div>
+    </PageContainer>
   );
 };
 
