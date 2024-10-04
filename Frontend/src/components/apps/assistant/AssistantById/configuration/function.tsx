ac070fe10eb89c6bfe7ed8b813e6bf052b97a36d
khaidinhh1234 <khaindph36466@fpt.edu.vn>
2024-10-04 14:41:54 +0700

    push

--- Frontend/src/components/apps/assistant/AssistantById/configuration/function.tsx
+++ Frontend/src/components/apps/assistant/AssistantById/configuration/function.tsx
@@ -1,12 +1,12 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { Avatar, Box, Button, Stack, Typography, useTheme } from '@mui/material';
+import { Avatar, Box, Stack, Typography, useTheme } from '@mui/material';
 import DashboardCard from 'src/components/shared/DashboardCard';
 import icon3Img from 'src/assets/images/svgs/icon-master-card.svg';
 import icon2Img from 'src/assets/images/svgs/icon-office-bag.svg';
 import icon1Img from 'src/assets/images/svgs/icon-paypal.svg';
 import icon4Img from 'src/assets/images/svgs/icon-pie.svg';
 import icon5Img from 'src/assets/images/svgs/icon-account.svg';
+import SimpleBar from 'simplebar-react'; // Import SimpleBar cho thanh cuộn tùy chỉnh
+import 'simplebar/dist/simplebar.min.css'; // Import CSS cho SimpleBar
 
 const Function = () => {
   const theme = useTheme();
@@ -18,6 +18,7 @@ const Function = () => {
   const error = theme.palette.error.main;
   const errorlight = theme.palette.error.light;
   const warninglight = theme.palette.warning.light;
+
   interface statType {
     title: string;
     level: number;
@@ -25,6 +26,7 @@ const Function = () => {
     lightcolor: string;
     icon: string;
   }
+
   const stats: statType[] = [
     {
       title: 'Function A',
@@ -61,47 +63,62 @@ const Function = () => {
       lightcolor: errorlight,
       icon: icon5Img,
     },
+    {
+      title: 'Function F',
+      level: 27.5,
+      color: error,
+      lightcolor: errorlight,
+      icon: icon4Img,
+    },
+    {
+      title: 'Function T',
+      level: 27.5,
+      color: error,
+      lightcolor: errorlight,
+      icon: icon1Img,
+    },
   ];
+
   return (
     <DashboardCard title="Function " subtitle="Function được trang bị cho trợ lý">
-      <Box>
-        <Stack spacing={3} mt={'26px'}>
-          {stats.map((stat, i) => (
-            <Stack
-              direction="row"
-              spacing={2}
-              justifyContent="space-between"
-              alignItems="center"
-              key={i}
-            >
-              <Stack direction="row" alignItems="center" spacing={2}>
-                <Avatar
-                  variant="rounded"
-                  sx={{
-                    bgcolor: stat.lightcolor,
-                    color: stat.color,
-                    width: 40,
-                    height: 40,
-                  }}
-                >
-                  <Avatar src={stat.icon} alt={stat.icon} sx={{ width: 24, height: 24 }} />
-                </Avatar>
-                <Box>
-                  <Typography variant="h6" mb="4px">
-                    {stat.title}
-                  </Typography>
-                  <Typography variant="subtitle2" color="textSecondary">
-                    {stat.level} MB
-                  </Typography>
-                </Box>
+      {/* Bọc nội dung trong SimpleBar để thêm thanh cuộn */}
+      <SimpleBar style={{ maxHeight: '410px', overflowX: 'hidden' }}>
+        <Box>
+          <Stack spacing={3} mt={'26px'}>
+            {stats.map((stat, i) => (
+              <Stack
+                direction="row"
+                spacing={2}
+                justifyContent="space-between"
+                alignItems="center"
+                key={i}
+              >
+                <Stack direction="row" alignItems="center" spacing={2}>
+                  <Avatar
+                    variant="rounded"
+                    sx={{
+                      bgcolor: stat.lightcolor,
+                      color: stat.color,
+                      width: 40,
+                      height: 40,
+                    }}
+                  >
+                    <Avatar src={stat.icon} alt={stat.icon} sx={{ width: 24, height: 24 }} />
+                  </Avatar>
+                  <Box>
+                    <Typography variant="h6" mb="4px">
+                      {stat.title}
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary">
+                      {stat.level} MB
+                    </Typography>
+                  </Box>
+                </Stack>
               </Stack>
-            </Stack>
-          ))}
-          <Button variant="outlined" color="primary" sx={{ mt: '40px !important' }}>
-            Xem toàn bộ
-          </Button>
-        </Stack>
-      </Box>
+            ))}
+          </Stack>
+        </Box>
+      </SimpleBar>
     </DashboardCard>
   );
 };
