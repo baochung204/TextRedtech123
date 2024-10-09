078309c440d877517324e16acf2a720ae152a9c8
BaoChung-Cloud <chungkbph38477@fpt.edu.vn>
2024-10-09 10:40:23 +0700

    new

--- Frontend/src/views/admin/buyproduct/index.tsx
+++ Frontend/src/views/admin/buyproduct/index.tsx
@@ -24,7 +24,7 @@ import product from 'src/assets/Adminphoto/san pham.png';
 import amountcheckout from 'src/assets/Adminphoto/so luong mua.png';
 import totalrevenue from 'src/assets/Adminphoto/tong doanh thu.png';
 import totalcheckout from 'src/assets/Adminphoto/tong thanh toan.png';
-import sale from 'src/assets/Adminphoto/so flash sale.png'
+import sale from 'src/assets/Adminphoto/so flash sale.png';
 import icontext, { default as iconPoint } from 'src/assets/images/logos/R-Point.png';
 import CustomTable from 'src/components/ComponentTables/CustomTable';
 import CustomTextField from 'src/components/forms/theme-elements/CustomTextField';
@@ -99,7 +99,6 @@ const DataBox = [
   },
   {
     bgColor: 'primary.light',
-<<<<<<< HEAD
     title: 'Tổng doanh thu',
     total: (
       <>
@@ -129,18 +128,12 @@ const DataBox = [
   },
   {
     bgColor: 'primary.light',
-=======
->>>>>>> 34d3ff34e865c52829dee216d741b86f07147b65
     title: 'Doanh thu',
     total: (
       <>
         <Box display="flex" alignItems="center" gap={0.4}>
-<<<<<<< HEAD
           <Typography variant="h6">12.423.423 </Typography>
           <img src={iconPoint} alt="" width={17} />
-=======
-          <Typography variant="h6">5.432.234đ</Typography>
->>>>>>> 34d3ff34e865c52829dee216d741b86f07147b65
         </Box>
       </>
     ),
@@ -319,7 +312,7 @@ const BuyPoints = () => {
               </IconButton>
             </Grid>
             <Grid item xs={4}>
-              <IconButton onClick={() => { }}>
+              <IconButton onClick={() => {}}>
                 <IconTrash stroke={2} style={{ color: '#FA896B' }} />
               </IconButton>
             </Grid>
