2696a1fd145ce2bc79d20cc16b7838e8868add56
ntoarn <nqton301004@gmail.com>
2024-10-12 03:49:13 +0700

    buonnguqua

---
+++ Frontend/src/views/admin/buyproduct/components/AddSearchProduct.tsx
@@ -0,0 +1,50 @@
+import { Box, Chip, Fab, Grid, Paper, Tooltip, Typography } from '@mui/material';
+
+import { IconPlus } from '@tabler/icons-react';
+import { useState } from 'react';
+import SimpleDialog from 'src/components/apps/assistant/AssistantEditor/dialog/searchDialog'
+// import SimpleDialog from 'src/components/apps/assistant/AssistantEditor/layout/addSearch'
+const AddSearchProduct = () => {
+  const [openFunction, setOpenFunction] = useState(false);
+  const handleClickOpenFunction = () => {
+    setOpenFunction(true);
+  };
+
+  return (
+    <Paper elevation={3} sx={{ minHeight: '50%',height:'190px', p: 2 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} lg={6} sx={{ display: 'flex', gap: 1 }}>
+          <Box fontWeight={600} mt={0.5}>
+            Tri thức
+            <Chip sx={{ ml: 1 }} size="small" label={"3/5"} color="primary" />
+          </Box>
+          
+        </Grid>
+        <Grid item xs={12} sm={6} lg={6} display={'flex'} justifyContent={'end'}>
+          {/* <Button
+            variant="contained"
+            color="primary"
+            component="span"
+            size="small"
+            style={{ marginBottom: '10px' }}
+            onClick={handleClickOpenSearch}
+          >
+            <AddIcon fontSize="small" style={{ marginRight: '10px' }} />
+          </Button> */}
+          <Tooltip title="Thêm File tri thức">
+            <Fab onClick={handleClickOpenFunction} size="small" color="primary" aria-label="plus">
+              <IconPlus width={18} />
+            </Fab>
+          </Tooltip>
+        </Grid>
+        <Grid item xs={12} sm={6} lg={12}>
+          <SimpleDialog openFunction={openFunction} setOpenFunction={setOpenFunction} />
+          <Typography sx={{ display: 'flex', justifyContent: 'end' }} fontSize={12} fontWeight={600}>500/1000 MB</Typography>
+        </Grid>
+        
+      </Grid>
+    </Paper>
+  );
+};
+
+export default AddSearchProduct;
--- Frontend/src/views/admin/buyproduct/components/SliderProduct.tsx
+++ Frontend/src/views/admin/buyproduct/components/SliderProduct.tsx
@@ -1,22 +1,23 @@
 import { Box, Button, Chip, Grid, Paper, Slider, Typography, useTheme } from '@mui/material';
 import { useState } from 'react';
 import CustomFormLabel from 'src/components/forms/theme-elements/CustomFormLabel';
- // or '@mui/system' if using system-based styling
 import DoneIcon from '@mui/icons-material/Done';
-// import { styled } from '@mui/material/styles'; 
+
 const valuetext = (value: any) => `${value}`;
 
 const SliderProduct = () => {
   // State để lưu giá trị của các slider
   const [fileCount, setFileCount] = useState<number>(0);
-  const [stepFile] = useState<number>(100);
-  const [stepStorage] = useState<number>(100000);
+  const [stepFile] = useState<number>(50); // Tối đa 50 tệp
+  const [stepStorage] = useState<number>(1024); // Tối đa 1028 MB
   const [storage, setStorage] = useState<number>(0);
   const [isClicked, setIsClicked] = useState(false);
   const theme = useTheme();
+
   const handleClick = () => {
     setIsClicked(true);
   };
+
   // Hàm xử lý khi thay đổi giá trị slider
   const handleFileCountChange = (_event: Event, newValue: number | number[]) => {
     setFileCount(newValue as number);
@@ -28,7 +29,7 @@ const SliderProduct = () => {
 
   return (
     <>
-      <Paper elevation={3} sx={{ minHeight: '5%',mt:2, px: 2, pt:0.1,pb:1.5 }}>
+      <Paper elevation={3} sx={{ minHeight: '5%', mt: 2, px: 2, pt: 0.1, pb: 1.5 }}>
         <Grid container my={2}>
           <Grid item xs={6}>
             <CustomFormLabel htmlFor="name" sx={{ mt: 0 }}>
@@ -36,10 +37,6 @@ const SliderProduct = () => {
             </CustomFormLabel>
           </Grid>
           <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'end' }}>
-            {' '}
-            {/* <Button variant="contained" color="primary" sx={{ p: 0 }}>
-              Lưu
-            </Button> */}
             <Button
               variant="contained"
               color="inherit"
@@ -87,12 +84,12 @@ const SliderProduct = () => {
             <Grid lg={6}>
               <Box fontWeight={500}>
                 Dung lượng
-                <Chip sx={{ ml: 1 }} size="small" label={stepStorage-storage +" MB"} color="primary" />
+                <Chip sx={{ ml: 1 }} size="small" label={`${stepStorage - storage} MB`} color="primary" />
               </Box>
             </Grid>
             <Grid lg={6}>
               <Box mr={1} display={'flex'} justifyContent={'end'}>
-                <Typography variant="subtitle2">{storage}MB</Typography>
+                <Typography variant="subtitle2">{storage} MB</Typography>
               </Box>
             </Grid>
           </Grid>
