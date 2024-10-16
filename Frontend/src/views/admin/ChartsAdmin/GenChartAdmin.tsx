3de889ea2876a98c5ed8811934e62a6fde4c9840
hoand <hoaace2003@gmail.com>
2024-10-16 13:57:45 +0700

    update

--- Frontend/src/views/admin/ChartsAdmin/GenChartAdmin.tsx
+++ Frontend/src/views/admin/ChartsAdmin/GenChartAdmin.tsx
@@ -61,7 +61,7 @@ const GenChartAdmin = ({ text, menuItems }: { text: any; menuItems: any }) => {
       categories: categories,
       labels: {
         show: true,
-        formatter: (value: string, opts?: any) => {
+        formatter: (value: string, _timestamp: string, opts?: any) => {
           const date = new Date(value);
           if (opts.i === 0 || opts.i === categories.length - 1) {
             return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
